36add1223b951ac5b8509846253fa8e9b5333611
nermeen mamdouh <nermeen.mamdouh44@gmail.com>
2022-08-30 23:59:17 +0200

    done

---
+++ src/components/CardDetails.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: "< Basic >",
+    btc: " 1 BTC ",
+    duration: "- Three Days ",
+    Views: "- Views ",
+    featured: "- Featured ",
+    quarters: "- Private Quarters ",
+  },
+  {
+    title: "< Suite >",
+    btc: "1 BTC",
+    duration: "- Five Days",
+    Views: "- Views",
+    featured: "- Featured",
+    quarters: "- Public Quarters",
+  },
+  {
+    title: "< Executive >",
+    btc: "1 BTC",
+    duration: "- Five Days",
+    Views: "- Views",
+    featured: "- Featured",
+    quarters: "- Private Quarters",
+  },
+];
---
+++ src/routes/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+import Video from "../components/Video";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <Video />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
