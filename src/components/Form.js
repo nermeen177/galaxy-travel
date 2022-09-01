faa3935d5582ce8e37623424115fcb52c2054456
nermeen mamdouh <nermeen.mamdouh44@gmail.com>
2022-09-01 04:45:48 +0200

    changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./Form.scss";
 
 const Form = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("Thanks, Submission is done");
+  }
   return (
     <div className="form-container">
       <div className="form-container__back-ground">
         <div className="form-container__layer"></div>
-        <form
-          className="contact-form"
-          onSubmit={() => alert("Thanks, Submission is done")}
-        >
+        <form className="contact-form" onSubmit={handleSubmit}>
           <h1 className="contact-form__header">Contact Us!</h1>
           <input
             type="text"
